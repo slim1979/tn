f6545c1a0483395b8d6f39f456c15065c8a7e6ec
slim1979 <chinezan@yandex.ru>
2017-05-31 12:29:53 +0300

    Пользователь может зарегистриироваться на ресурсе

---
+++ spec/acceptance/user_registration_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User can past registration', %q{
+  In order to ask the questions
+  As an user
+  I want to past registration
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'User past registration on site' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user_1@test.com'
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
